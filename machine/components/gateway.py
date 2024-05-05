10efb93e50c66e68f49eee82f95edd2d04e71af4
happex <110610727+okayhappex@users.noreply.github.com>
2024-05-05 18:42:03 +0200

    Add another parsing method

--- machine/components/gateway.py
+++ machine/components/gateway.py
@@ -1,5 +1,15 @@
+import json
+
 def split(content: str) -> list[str]:
     return content.split('\n')
 
-def parse(content: list, keys: tuple) -> list[dict]:
-    return [ dict(zip(keys, item.split('::'))) for item in content ]
+def parse(content: list, keys: tuple | None = None) -> list[dict] | dict:
+    if keys is None:
+        data = { item.split('=')[0] : item.split('=')[1] for item in content }
+        
+        for param, val in data.items():
+            data[param] = json.loads(val)
+        
+        return data
+    else:
+        return [ dict(zip(keys, item.split('::'))) for item in content ]
